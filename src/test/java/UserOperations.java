d5aa8762841ad69c2866fe92d5d93dff10d5b085
Cleison F. Scolari <cleisonnoob@hotmail.com>
2022-11-23 00:26:44 -0300

    Teste de criacao de usuario

--- src/test/java/UserOperations.java
+++ src/test/java/UserOperations.java
@@ -1,14 +1,16 @@
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import  java.util.*;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.*;
 
 public class UserOperations {
 
-    public static String baseUrl = "https://petstore.swagger.io/v2/swagger.json";
+   // public static String baseUrl = "https://petstore.swagger.io/v2/swagger.json";
 
     @Test
     public void testMetaDados(){
@@ -19,5 +21,17 @@ public class UserOperations {
                 .body("page", is(2))
                 .body("data", is(notNullValue()));
 
+    }
+
+    @Test
+    public void criarUsuario(){
+        given().log().all().
+                contentType(ContentType.JSON).
+               body("{\"name\": \"Tunico\", \"job\": \"progamador\" }").
+        when().
+                post("https://reqres.in/api/users").
+        then().
+                statusCode(HttpStatus.SC_CREATED).
+                body("name", is("Tunico"));
     }
 }
