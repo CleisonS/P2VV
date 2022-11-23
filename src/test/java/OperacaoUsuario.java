b5b087b46cc0c25e378afdc6b0c3052f9335494c
Cleison F. Scolari <cleisonnoob@hotmail.com>
2022-11-23 02:56:53 -0300

    Urls funcionais com baseURI

--- src/test/java/OperacaoUsuario.java
+++ src/test/java/OperacaoUsuario.java
@@ -1,20 +1,21 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testng.annotations.BeforeClass;
+
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.*;
 
-public class UserOperations {
+public class OperacaoUsuario {
 
-    @BeforeClass
+    @BeforeAll
     public static void setUp(){
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-        //log de respostas caso as validações falhem
-        RestAssured.baseURI = "https://reqres.in";// não ta puxando pelo baseURI
-        RestAssured.basePath = "/api";
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();//log de respostas caso as validações falhem
+        baseURI = "https://reqres.in";//
+       basePath = "/api";
     }
 
     @Test
@@ -24,7 +25,7 @@ public class UserOperations {
                 //param("page","2").
                 params("page", "2").
         when().
-                get("https://reqres.in/api/users").//era pra puxar do baseURI
+                get("/users").//
         then().
                 statusCode(HttpStatus.SC_OK)
                 .body("page", is(2))
