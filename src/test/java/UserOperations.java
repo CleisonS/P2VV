98ddf3195049bbece08e991b6e33261bd23305da
Cleison F. Scolari <cleisonnoob@hotmail.com>
2022-11-22 22:55:52 -0300

    Teste de listar

--- src/test/java/UserOperations.java
+++ src/test/java/UserOperations.java
@@ -1,9 +1,11 @@
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import  java.util.*;
 
 import static io.restassured.RestAssured.when;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
+
 public class UserOperations {
 
     public static String baseUrl = "https://petstore.swagger.io/v2/swagger.json";
@@ -13,8 +15,9 @@ public class UserOperations {
         when().
                 get("https://reqres.in/api/users?page=2").
         then().
-                statusCode(200)
-                .body("page", is(2));
+                statusCode(HttpStatus.SC_OK)
+                .body("page", is(2))
+                .body("data", is(notNullValue()));
 
     }
 }
